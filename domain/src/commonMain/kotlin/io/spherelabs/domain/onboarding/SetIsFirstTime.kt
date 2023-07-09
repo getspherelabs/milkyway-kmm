22654e63dd094d343dfd41c63e185ce767df6356
behzoddev <behzoddev@gmail.com>
2023-07-09 23:21:33 +0900

    feat: Add SetIsFirstTime use case

---
+++ domain/src/commonMain/kotlin/io/spherelabs/domain/onboarding/SetIsFirstTime.kt
@@ -0,0 +1,17 @@
+package io.spherelabs.domain.onboarding
+
+import io.spherelabs.setting.onboarding.OnboardingSetting
+
+interface SetIsFirstTime {
+    suspend fun execute(value: Boolean)
+}
+
+
+class DefaultSetIsFirstTime(
+    private val onboardingSetting: OnboardingSetting
+) : SetIsFirstTime {
+
+    override suspend fun execute(value: Boolean) {
+        onboardingSetting.setIsFirstTime(value)
+    }
+}
